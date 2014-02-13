16dcc654ef045d749f8bb094d4b78079b1533dd0
James Fairbanks <james.fairbanks@gatech.edu>
2014-02-13 15:52:46 -0500

    Finding dependency and patching the imports.

--- split_scan.py
+++ split_scan.py
@@ -1,7 +1,12 @@
-""" This module demonstrates some features of the libstreampy 
+#!/usr/bin/env python
+""" This module demonstrates some features of the libstreampy
 We take an iterable and split it into even and odd then scan it
 
-Comparing a static implementation to the stream.py implementation"""
+Comparing a static implementation to the stream.py implementation
+
+Depends on the stream module from https://github.com/aht/stream.py.
+"""
+
 import sys
 sys.path.append("~/Code/streaming/libstreampy/")
 from time import time
@@ -13,56 +18,61 @@ from stream import item
 import operator as ops
 import numpy
 
-N = int(sys.argv[1])
-instream = range(N)
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: ./split_scan.py N")
+        sys.exit(1)
 
-#use a python scan impementation no cheating for static team
-def scan(op, seq):
-    """Compute the op-scan of seq"""
-    l = []
-    s = 0
-    for x in seq:
-	s = op(s,x)
-	l.append(s)
-    return l
-#static
-ts = time()
-#filter, scan
-evens = instream[::2]
-odds = instream[1::2]
-even_ans = scan(ops.add, evens)
-odd_ans = scan(ops.add, odds )
-static_time = time() - ts
+    N = int(sys.argv[1])
+    instream = range(N)
 
-#streaming
-#these just create objects that are going to accept streams
-cong_2 = lambda x: x%2==0
-evens = filter(cong_2)
-odds  = filter(lambda x: not cong_2(x))
-ts = time()
-#do the wiring
-instream >> tee(evens)
-instream >> odds
-seans = evens >> fold(ops.add)
-soans = odds  >> fold(ops.add)
-#seans = evens >> stream.reduce(ops.add)
-#soans = odds  >> stream.reduce(ops.add)
+    #use a python scan impementation no cheating for static team
+    def scan(op, seq):
+        """Compute the op-scan of seq"""
+        l = []
+        s = 0
+        for x in seq:
+            s = op(s,x)
+            l.append(s)
+        return l
+    #static
+    ts = time()
+    #filter, scan
+    evens = instream[::2]
+    odds = instream[1::2]
+    even_ans = scan(ops.add, evens)
+    odd_ans = scan(ops.add, odds )
+    static_time = time() - ts
 
-# trigger execution
-seans = seans >> item[:]
-soans = soans >> item[:]
-stream_time = time() - ts
-#print(even_ans)
-#print(seans >> item[:])
-#print(odd_ans)
-#print(soans >> item[:])
+    #streaming
+    #these just create objects that are going to accept streams
+    cong_2 = lambda x: x%2==0
+    evens = filter(cong_2)
+    odds  = filter(lambda x: not cong_2(x))
+    ts = time()
+    #do the wiring
+    instream >> tee(evens)
+    instream >> odds
+    seans = evens >> fold(ops.add)
+    soans = odds  >> fold(ops.add)
+    #seans = evens >> stream.reduce(ops.add)
+    #soans = odds  >> stream.reduce(ops.add)
+
+    # trigger execution
+    seans = seans >> item[:]
+    soans = soans >> item[:]
+    stream_time = time() - ts
+    #print(even_ans)
+    #print(seans >> item[:])
+    #print(odd_ans)
+    #print(soans >> item[:])
 
 
-#make sure we got the right answer
-assert ( seans == list(even_ans))
-assert ( soans == list(odd_ans))
-#show us what the performance penalty for using streams was.
-print("we got the same answer using the streams")
-print("static_time: %f, ips: %f" % (static_time, N//static_time))
-print("stream_time: %f, ips: %f" % (stream_time, N//stream_time))
-print("streaming/static: %f" % (stream_time/static_time))
+    #make sure we got the right answer
+    assert ( seans == list(even_ans))
+    assert ( soans == list(odd_ans))
+    #show us what the performance penalty for using streams was.
+    print("we got the same answer using the streams")
+    print("static_time: %f, ips: %f" % (static_time, N//static_time))
+    print("stream_time: %f, ips: %f" % (stream_time, N//stream_time))
+    print("streaming/static: %f" % (stream_time/static_time))
---
+++ stream.py
@@ -0,0 +1,1 @@
+libstreampy/stream.py
