11e809a700d42eeaa6e7b4acfd7494313428116e
James Fairbanks <james.fairbanks@gatech.edu>
2014-02-14 18:02:41 -0500

    refactor to enable reuse.

--- arma.py
+++ arma.py
@@ -1,9 +1,16 @@
 import sys
 import random
 
-fp = sys.stdin
-a_0 = 1.0/2
-for line in fp:
-    x = float(line)
-    state = a_0 * state + x
-    print(state)
+def arma_gen(stream:iter, consts):
+    """Generator function that produces an ARMA stream."""
+    state = 0
+    for line in stream:
+        x = float(line)
+        state = a_0 * state + x
+        yield state
+
+if __name__ == '__main__':
+    fp = sys.stdin
+    ofp = sys.stdout
+    for y in arma_gen(fp, -1.0/2):
+        print(y, file=ofp)
